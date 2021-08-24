1d81e558a97cdb5cf4323a0ab25bbed7732a2a92
felix <472252510@qq.com>
2021-08-24 17:39:12 +0800

    feat：202.快乐数

---
+++ HashTable/202.快乐数.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=202 lang=typescript
+ *
+ * [202] 快乐数
+ * Set的方式
+ */
+
+// @lc code=start
+function isHappy(n: number): boolean {
+  const sumSet = new Set()
+  const handleN = (num: number): number => {
+    if (num === 1 || num === 0) return n
+    let res = 0
+    // 开始快乐
+    while (num) {
+      // 个位数平方
+      res += (num % 10) ** 2
+      // 继续递归十位数
+      num = Math.floor(num / 10)
+    }
+    return res
+  }
+  while (n !== 1 && !sumSet.has(n)) {
+    sumSet.add(n)
+    n = handleN(n)
+  }
+  return n === 1
+};
+isHappy(19)
+// @lc code=end
+
