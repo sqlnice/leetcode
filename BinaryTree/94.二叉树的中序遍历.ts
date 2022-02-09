b7ef9e9eb662b1cce72fd99bcacba1f0da41778d
sqlnice <472252510@qq.com>
2022-02-09 15:17:49 +0800

    feat：94.二叉树的中序遍历

--- BinaryTree/94.二叉树的中序遍历.ts
+++ BinaryTree/94.二叉树的中序遍历.ts
@@ -19,17 +19,68 @@
  * }
  */
 
+// 递归
+// function inorderTraversal(root: TreeNode | null): number[] {
+//   const res: number[] = [];
+//   const inorder = (root: TreeNode) => {
+//     if (!root) {
+//       return;
+//     }
+//     inorder(root.left);
+//     res.push(root.val);
+//     inorder(root.right);
+//   };
+//   inorder(root);
+//   return res;
+// }
+
+// function inorderTraversal(root: TreeNode | null): number[] {
+//   const result: number[] = [];
+//   if (!root) return result;
+//   const stack = [];
+
+//   // 把左边全部入栈
+//   while (root) {
+//     stack.push(root);
+//     root = root.left;
+//   }
+
+//   while (stack.length) {
+//     // 取出栈顶的元素
+//     let node = stack.pop();
+//     result.push(node.val);
+//     // 取右子树
+//     node = node.right;
+//     while (node) {
+//       stack.push(node);
+//       // 不断压入右子树的左节点
+//       node = node.left;
+//     }
+//   }
+//   return result;
+// }
+
+// 迭代（栈
+// 进栈 左 -> 右
+// 出站 左 -> 中 -> 右
 function inorderTraversal(root: TreeNode | null): number[] {
-  const res: number[] = [];
-  const inorder = (root: TreeNode) => {
-    if (!root) {
-      return;
+  const res = [];
+  const stack = [];
+  let cur = root;
+  while (stack.length || cur) {
+    if (cur) {
+      stack.push(cur);
+      // 左
+      cur = cur.left;
+    } else {
+      // --> 弹出 中
+      cur = stack.pop();
+      res.push(cur.val);
+      // 右
+      cur = cur.right;
     }
-    inorder(root.left);
-    res.push(root.val);
-    inorder(root.right);
-  };
-  inorder(root);
+  }
   return res;
 }
+
 // @lc code=end
