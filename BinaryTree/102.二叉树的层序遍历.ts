6e4355136fa352ba5cd4650242190587e758391a
sqlnice <472252510@qq.com>
2022-02-09 15:18:21 +0800

    feat：102.二叉树的层序遍历

---
+++ BinaryTree/102.二叉树的层序遍历.ts
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=typescript
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+  // 层序遍历
+  const res: number[][] = [];
+  const queue: TreeNode[] = [];
+  queue.push(root);
+  if (!root) return res;
+  while (queue.length) {
+    // 当前层级节点数
+    let length = queue.length;
+    // 存放每一层的节点
+    let curLevel = [];
+    for (let i = 0; i < length; i++) {
+      let node = queue.shift();
+      curLevel.push(node.val);
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    res.push(curLevel);
+  }
+  return res;
+}
+// @lc code=end
