fbbed4245d37d72968e6d12c90911ddbf55cf8a3
sqlnice <472252510@qq.com>
2022-02-09 15:18:05 +0800

    feat：144.二叉树的前序遍历

--- BinaryTree/144.二叉树的前序遍历.ts
+++ BinaryTree/144.二叉树的前序遍历.ts
@@ -16,16 +16,35 @@ class TreeNode {
     this.right = right === undefined ? null : right;
   }
 }
+// 递归
+// function preorderTraversal(root: TreeNode | null): number[] {
+//   const res: number[] = [];
+//   const preorder = (root: TreeNode) => {
+//     if (!root) return;
+//     res.push(root.val);
+//     preorder(root.left);
+//     preorder(root.right);
+//   };
+//   preorder(root);
+//   return res;
+// }
 
+// 迭代（栈
+// 进栈 右 -> 左
+// 出站 中 -> 左 -> 右
 function preorderTraversal(root: TreeNode | null): number[] {
-  const res: number[] = [];
-  const preorder = (root: TreeNode) => {
-    if (!root) return;
-    res.push(root.val);
-    preorder(root.left);
-    preorder(root.right);
-  };
-  preorder(root);
-  return res;
+  const result: number[] = [];
+  if (!root) return result;
+  const stack = [root];
+  let cur = null;
+  while (stack.length) {
+    cur = stack.pop();
+    result.push(cur.val);
+    cur.right && stack.push(cur.right);
+    cur.left && stack.push(cur.left);
+  }
+
+  return result;
 }
+
 // @lc code=end
