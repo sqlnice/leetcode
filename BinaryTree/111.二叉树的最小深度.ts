b34d1562237320a479fbfc49ec3e0eb034c0f538
sqlnice <472252510@qq.com>
2022-03-04 17:58:43 +0800

    feat：111.二叉树的最小深度

--- BinaryTree/111.二叉树的最小深度.ts
+++ BinaryTree/111.二叉树的最小深度.ts
@@ -19,23 +19,43 @@
  * }
  */
 
+// 迭代法
+// function minDepth(root: TreeNode | null): number {
+//   let res = 0;
+//   if (!root) return res;
+//   const queue = [root];
+//   while (queue.length) {
+//     let length = queue.length;
+//     res++;
+//     while (length--) {
+//       const node = queue.shift();
+//       node.left && queue.push(node.left);
+//       node.right && queue.push(node.right);
+//       // 都为空的情况下就是最小
+//       if (!node.left && !node.right) {
+//         return res;
+//       }
+//     }
+//   }
+//   return res;
+// }
+
+// 递归法
 function minDepth(root: TreeNode | null): number {
-  let res = 0;
-  if (!root) return res;
-  const queue = [root];
-  while (queue.length) {
-    let length = queue.length;
-    res++;
-    while (length--) {
-      const node = queue.shift();
-      node.left && queue.push(node.left);
-      node.right && queue.push(node.right);
-      // 都为空的情况下就是最小
-      if (!node.left && !node.right) {
-        return res;
-      }
+  // 1.确定函数的参数和返回值
+  const getMinDepth = (node: TreeNode | null): number => {
+    // 2.确定终止条件
+    if (!root) return 0;
+    // 3.确定单层递归的逻辑
+    if (!node.left && !node.right) return 1;
+    if (node.left && !node.right) {
+      return 1 + getMinDepth(node.left);
     }
-  }
-  return res;
+    if (!node.left && node.right) {
+      return 1 + getMinDepth(node.right);
+    }
+    return Math.min(getMinDepth(node.left), getMinDepth(node.right)) + 1;
+  };
+  return getMinDepth(root);
 }
 // @lc code=end
