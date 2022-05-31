1ed8a9a514cacea9c1eee95cac4d6fd75d713a84
sqlnice <472252510@qq.com>
2022-05-31 12:01:12 +0800

    feat: 53.最大子数组和

---
+++ Greedy/53.最大子数组和.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=typescript
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+function maxSubArray(nums: number[]): number {
+  let result = -Infinity;
+  let count = 0;
+  for (let i = 0; i <= nums.length; i++) {
+    count += nums[i];
+    if (count > result) {
+      result = count;
+    }
+    if (count < 0) {
+      count = 0;
+    }
+  }
+  return result;
+}
+// @lc code=end
