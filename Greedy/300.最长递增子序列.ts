5e66617d9d062b7d18f1e746b11a1273cd71d8ef
sqlnice <472252510@qq.com>
2022-07-26 10:47:21 +0800

    feat: 300.最长递增子序列 动态规划版本

---
+++ Greedy/300.最长递增子序列.ts
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=typescript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+function lengthOfLIS(nums: number[]): number {
+  // 动态规划
+  const dp = new Array(nums.length).fill(1);
+  let max = 1;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[j] + 1, dp[i]);
+      }
+    }
+    max = Math.max(max, dp[i]);
+  }
+
+  return max;
+}
+// @lc code=end
